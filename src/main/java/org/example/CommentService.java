35a837f9b48f084e7189a72892955ffc7fdefdd3
asrytikov <asrytikov@gmail.com>
2023-12-03 16:58:32 +0400

    Lesson 30

--- src/main/java/org/example/CommentService.java
+++ src/main/java/org/example/CommentService.java
@@ -1,2 +1,17 @@
-package org.example;public class CommentService {
+package org.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentService {
+    private final CommentRepository commentRepository;
+
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    public CommentRepository getCommentRepository() {
+        return commentRepository;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,17 @@
-package org.example;public class Main {
+package org.example;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+       var cs1 = context.getBean(CommentService.class);
+       var cs2 = context.getBean(UserService.class);
+
+       boolean b1 = cs1.getCommentRepository() == cs2.getCommentRepository();
+        System.out.println(b1);
+        System.out.println(cs1.getCommentRepository());
+        System.out.println(cs2.getCommentRepository());
+
     }
 }
